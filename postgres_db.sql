98c6932726cfcf22a46c6b1cd8cf469dd88401ed
vinay-curatio <vinay@curatio.me>
2018-02-12 09:24:02 -0800

    Added basic DB setup including a calculations table

---
+++ postgres_db.sql
@@ -0,0 +1,66 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.1
+-- Dumped by pg_dump version 10.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: postgres; Type: COMMENT; Schema: -; Owner: vinaybalaji
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: calculations; Type: TABLE; Schema: public; Owner: my_admin
+--
+
+CREATE TABLE calculations (
+    calculations text
+);
+
+
+ALTER TABLE calculations OWNER TO my_admin;
+
+--
+-- Data for Name: calculations; Type: TABLE DATA; Schema: public; Owner: my_admin
+--
+
+COPY calculations (calculations) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
